ff079475d6d3f3bfaa4a20f51aa5265e81c81334
xavier sabree <sabreexavier@gmail.com>
2023-08-20 21:14:14 -0400

    make it better

--- home/static/js/drawer.js
+++
@@ -1,7 +0,0 @@
-// JavaScript to toggle the drawer
-const toggleButton = document.getElementById('toggleDrawer');
-const drawer = document.getElementById('drawer');
-
-toggleButton.addEventListener('click', () => {
-    drawer.classList.toggle('open');
-});
