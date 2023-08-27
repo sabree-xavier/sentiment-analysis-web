92b1886be3483927b73b4263432004753220b28d
xavier sabree <sabreexavier@gmail.com>
2023-08-27 00:42:08 -0400

    vercel compatibility

--- sentiment_project/settings.py
+++ sentiment_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-cenpix#qb!ra8i7%vrlrl_4t)bl#wgfdy_6ed9%j606xrw46ml
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
